ef5c9c266fd66e1968ab9b35b3da4f98b651a6bc
Carlos Caraccia <carloscaraccia@gmail.com>
2020-11-04 09:46:24 -0300

    Refactored main pallet builder view. To allow a clearer reading experience

--- LectorDeCamara/Views/PalletBuilderMainView.swift
+++ LectorDeCamara/Views/PalletBuilderMainView.swift
@@ -9,6 +9,27 @@ import SwiftUI
 import CoreData
 import Introspect
 
+struct PalletBuilderMainViewCell:View {
+    
+    var pallet:Pallet
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Date: \(pallet.fechaInString!)")
+                Spacer()
+                Text("id: \(pallet.numero)" as String)
+            }
+            .padding(.bottom, 10)
+            HStack {
+                Text("Boxes: \(pallet.cajas.count) u")
+                Spacer()
+                Text("Wt.: \(pallet.pesoNetoDeCajas) kg" as String)
+            }
+        }
+    }
+}
+
 struct PalletBuilderMainView: View {
     
     @Environment(\.managedObjectContext) var viewContext:NSManagedObjectContext
@@ -22,9 +43,12 @@ struct PalletBuilderMainView: View {
     @State private var tableView:UITableView?
     
     init() {
+        // we create the fetch request in here because otherwise our code will be a mess
         let request = NSFetchRequest<Pallet>(entityName: "Pallet")
         request.predicate = NSPredicate(format: "TRUEPREDICATE")
         request.sortDescriptors = [NSSortDescriptor(key: "fecha", ascending: false)]
+        // the projected value of @FetchRequest is a FetchRequest, that can be initialized in
+        // cleaner way
         _pallets = FetchRequest(fetchRequest: request)
     }
     
@@ -40,34 +64,24 @@ struct PalletBuilderMainView: View {
                 List {
                     ForEach(pallets, id:\.numero) { pallet in
                         NavigationLink(destination: PalletDetailsView(numeroDePallet: pallet.numero).navigationBarTitle("Pallet: \(pallet.numero)")) {
-                            VStack {
-                                HStack {
-                                    Text("Date: \(pallet.fechaInString!)")
-                                    Spacer()
-                                    Text("id: \(pallet.numero)" as String)
-                                }
-                                .padding(.bottom, 10)
-                                HStack {
-                                    Text("Boxes: \(pallet.cajas.count) u")
-                                    Spacer()
-                                    Text("Wt.: \(pallet.pesoNetoDeCajas) kg" as String)
-                                }
-                            }
+                            PalletBuilderMainViewCell(pallet: pallet)
                         }
                         .font(.system(size: 14, weight: .light, design: .monospaced))
                         .onAppear(perform: {
+                            // hack to solve unselection of cells
                             deselectRows()
                         })
                     }.onDelete(perform: { indexSet in
                         indexSet.map{pallets[$0]}.forEach { (pallet) in
-                            // we delete the pallet but the boxes are still in the database
-                            // this is because the delete rule in the relationship is nullify
+                            // we delete the pallet but the boxes will be kept in the database
+                            // because the delete rule in the relationship is nullify.
                             viewContext.delete(pallet)
                             try? viewContext.save()
                         }
                     })
                 }
                 .introspectTableView { (tableView) in
+                    // hack to solve unselection of cells
                     self.tableView = tableView
                 }
             }
@@ -82,6 +96,7 @@ struct PalletBuilderMainView: View {
         }
     }
     
+    /// function used to desect rows after being selected by user.
     private func deselectRows() {
         if let tableView = tableView, let selectedRow = tableView.indexPathForSelectedRow {
             tableView.deselectRow(at: selectedRow, animated: false)
