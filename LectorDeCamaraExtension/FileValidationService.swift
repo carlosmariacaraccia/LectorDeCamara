9af8c5b0f2bad91e5cd10d757fed6d37e6e929f9
Carlos Maria Caraccia <carloscaraccia@gmail.com>
2021-01-12 13:39:55 -0300

    Added file validations

---
+++ LectorDeCamaraExtension/FileValidationService.swift
@@ -0,0 +1,8 @@
+//
+//  FileValidationError.swift
+//  Lector de camara
+//
+//  Created by Carlos Caraccia on 1/11/21.
+//
+
+import Foundation
---
+++ LectorDeCamaraExtension/UIViewController+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController+Extension.swift
+//  Lector de camaraExtension
+//
+//  Created by Carlos Caraccia on 1/11/21.
+//
+
+import Foundation
